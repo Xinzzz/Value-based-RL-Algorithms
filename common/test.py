174e8a37e59861bbf0e2d63df88a1e9caa2407cc
Xinzzz <296232375@qq.com>
2020-02-29 21:13:19 +0800

    02/29

--- common/test.py
+++ common/test.py
@@ -14,9 +14,66 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
 
-env = gym.make('LunarLander-v2').unwrapped
 
+env = gym.make('CartPole-v0').unwrapped
+
+
+plt.ion()
+
+# if gpu is to be used
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+class DQN(nn.Module):
+
+    def __init__(self, h, w, outputs):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
+
+        # Number of Linear input connections depends on output of conv2d layers
+        # and therefore the input image size, so compute it.
+        def conv2d_size_out(size, kernel_size = 5, stride = 2):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
+        linear_input_size = convw * convh * 32
+        self.head = nn.Linear(linear_input_size, outputs)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        return self.head(x.view(x.size(0), -1))
 
 resize = T.Compose([T.ToPILImage(),
                     T.Resize(40, interpolation=Image.CUBIC),
@@ -31,38 +88,47 @@ def get_cart_location(screen_width):
 def get_screen():
     # Returned screen requested by gym is 400x600x3, but is sometimes larger
     # such as 800x1200x3. Transpose it into torch order (CHW).
-    env.reset()
-    for _ in range(20):
-        env.render()
-        env.step(env.action_space.sample())
     screen = env.render(mode='rgb_array').transpose((2, 0, 1))
-    # # Cart is in the lower half, so strip off the top and bottom of the screen
-    # _, screen_height, screen_width = screen.shape
-    # screen = screen[:, int(screen_height*0.4):int(screen_height * 0.8)]
-    # view_width = int(screen_width * 0.6)
-    # cart_location = get_cart_location(screen_width)
-    # if cart_location < view_width // 2:
-    #     slice_range = slice(view_width)
-    # elif cart_location > (screen_width - view_width // 2):
-    #     slice_range = slice(-view_width, None)
-    # else:
-    #     slice_range = slice(cart_location - view_width // 2,
-    #                         cart_location + view_width // 2)
-    # # Strip off the edges, so that we have a square image centered on a cart
-    # screen = screen[:, :, slice_range]
-    # # Convert to float, rescale, convert to torch tensor
-    # # (this doesn't require a copy)
-    screen = np.ascontiguousarray(screen, dtype=np.float32)/255
+    # Cart is in the lower half, so strip off the top and bottom of the screen
+    _, screen_height, screen_width = screen.shape
+    screen = screen[:, int(screen_height*0.4):int(screen_height * 0.8)]
+    view_width = int(screen_width * 0.6)
+    cart_location = get_cart_location(screen_width)
+    if cart_location < view_width // 2:
+        slice_range = slice(view_width)
+    elif cart_location > (screen_width - view_width // 2):
+        slice_range = slice(-view_width, None)
+    else:
+        slice_range = slice(cart_location - view_width // 2,
+                            cart_location + view_width // 2)
+    # Strip off the edges, so that we have a square image centered on a cart
+    screen = screen[:, :, slice_range]
+    # Convert to float, rescale, convert to torch tensor
+    # (this doesn't require a copy)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
     screen = torch.from_numpy(screen)
     # Resize, and add a batch dimension (BCHW)
-    return screen.unsqueeze(0).to(device)
+    return resize(screen).unsqueeze(0).to(device)
 
 
 env.reset()
-plt.tight_layout()
-plt.figure()
-plt.imshow(get_screen().cpu().squeeze(0).permute(1, 2, 0).numpy(),
-           interpolation='none')
-plt.axis('off')
-plt.savefig('test.svg', bbox_inches='tight', facecolor='none', edgecolor='none', dpi=300)
-plt.show()
+
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+TARGET_UPDATE = 10
+
+# Get screen size so that we can initialize layers correctly based on shape
+# returned from AI gym. Typical dimensions at this point are close to 3x40x90
+# which is the result of a clamped and down-scaled render buffer in get_screen()
+init_screen = get_screen()
+_, _, screen_height, screen_width = init_screen.shape
+
+# Get number of actions from gym action space
+n_actions = env.action_space.n
+
+policy_net = DQN(screen_height, screen_width, n_actions).to(device)
+
+print(policy_net)
--- main.py
+++ main.py
@@ -21,52 +21,52 @@ def ready_env(env_id):
     seed_torch(seed)
     env.seed(seed)
 
-ready_env('monte')
+ready_env('cartpole')
 agent_1 = DQNAgent(env, params, 64,64)
-# agent_2 = DQNAgent(env, params, 64,128)
-# agent_3 = DQNAgent(env, params, 128,128)
-# agent_4 = DQNAgent(env, params, 128,256)
-# agent_5 = DQNAgent(env, params, 256,256)
-# agent_6 = DQNAgent(env, params, 256,512)
+agent_2 = DQNAgent(env, params, 64,128)
+agent_3 = DQNAgent(env, params, 128,128)
+agent_4 = DQNAgent(env, params, 128,256)
+agent_5 = DQNAgent(env, params, 256,256)
+agent_6 = DQNAgent(env, params, 256,512)
 
-# agent_7 = DQNAgent(env, params, 64,64, noisy=True)
-# agent_8 = DQNAgent(env, params, 64,128, noisy=True)
-# agent_9 = DQNAgent(env, params, 128,128, noisy=True)
-# agent_10 = DQNAgent(env, params, 128,256, noisy=True)
-# agent_11 = DQNAgent(env, params, 256,256, noisy=True)
-# agent_12 = DQNAgent(env, params, 256,512, noisy=True)
+agent_7 = DQNAgent(env, params, 64,64, noisy=True)
+agent_8 = DQNAgent(env, params, 64,128, noisy=True)
+agent_9 = DQNAgent(env, params, 128,128, noisy=True)
+agent_10 = DQNAgent(env, params, 128,256, noisy=True)
+agent_11 = DQNAgent(env, params, 256,256, noisy=True)
+agent_12 = DQNAgent(env, params, 256,512, noisy=True)
 
-# agent_13 = DQNAgent(env, params, 64,64, dueling=True)
-# agent_14 = DQNAgent(env, params, 64,128, dueling=True)
-# agent_15 = DQNAgent(env, params, 128,128, dueling=True)
-# agent_16 = DQNAgent(env, params, 128,256, dueling=True)
-# agent_17 = DQNAgent(env, params, 256,256, dueling=True)
-# agent_18 = DQNAgent(env, params, 256,512, dueling=True)
+agent_13 = DQNAgent(env, params, 64,64, dueling=True)
+agent_14 = DQNAgent(env, params, 64,128, dueling=True)
+agent_15 = DQNAgent(env, params, 128,128, dueling=True)
+agent_16 = DQNAgent(env, params, 128,256, dueling=True)
+agent_17 = DQNAgent(env, params, 256,256, dueling=True)
+agent_18 = DQNAgent(env, params, 256,512, dueling=True)
 
-# agent_19 = DQNAgent(env, params, 64,64, noisy=True, dueling=True)
-# agent_20 = DQNAgent(env, params, 64,128, noisy=True, dueling=True)
-# agent_21 = DQNAgent(env, params, 128,128, noisy=True, dueling=True)
-# agent_22 = DQNAgent(env, params, 128,256, noisy=True, dueling=True)
-# agent_23 = DQNAgent(env, params, 256,256, noisy=True, dueling=True)
-# agent_24 = DQNAgent(env, params, 256,512, noisy=True, dueling=True)
+agent_19 = DQNAgent(env, params, 64,64, noisy=True, dueling=True)
+agent_20 = DQNAgent(env, params, 64,128, noisy=True, dueling=True)
+agent_21 = DQNAgent(env, params, 128,128, noisy=True, dueling=True)
+agent_22 = DQNAgent(env, params, 128,256, noisy=True, dueling=True)
+agent_23 = DQNAgent(env, params, 256,256, noisy=True, dueling=True)
+agent_24 = DQNAgent(env, params, 256,512, noisy=True, dueling=True)
 
-# agent_25 = DQNAgent(env, params, 64,64, noisy=True, dueling=True, mod=True)
-# agent_26 = DQNAgent(env, params, 64,128, noisy=True, dueling=True, mod=True)
-# agent_27 = DQNAgent(env, params, 128,128, noisy=True, dueling=True, mod=True)
-# agent_28 = DQNAgent(env, params, 128,256, noisy=True, dueling=True, mod=True)
-# agent_29 = DQNAgent(env, params, 256,256, noisy=True, dueling=True, mod=True)
-# agent_30 = DQNAgent(env, params, 256,512, noisy=True, dueling=True, mod=True)
+agent_25 = DQNAgent(env, params, 64,64, noisy=True, dueling=True, mod=True)
+agent_26 = DQNAgent(env, params, 64,128, noisy=True, dueling=True, mod=True)
+agent_27 = DQNAgent(env, params, 128,128, noisy=True, dueling=True, mod=True)
+agent_28 = DQNAgent(env, params, 128,256, noisy=True, dueling=True, mod=True)
+agent_29 = DQNAgent(env, params, 256,256, noisy=True, dueling=True, mod=True)
+agent_30 = DQNAgent(env, params, 256,512, noisy=True, dueling=True, mod=True)
 
-# agent_31 = DQNAgent(env, params, 64,64, noisy=True, mod=True)
-# agent_32 = DQNAgent(env, params, 64,128, noisy=True, mod=True)
-# agent_33 = DQNAgent(env, params, 128,128, noisy=True, mod=True)
-# agent_34 = DQNAgent(env, params, 128,256, noisy=True, mod=True)
-# agent_35 = DQNAgent(env, params, 256,256, noisy=True, mod=True)
-# agent_36 = DQNAgent(env, params, 256,512, noisy=True, mod=True)
+agent_31 = DQNAgent(env, params, 64,64, noisy=True, mod=True)
+agent_32 = DQNAgent(env, params, 64,128, noisy=True, mod=True)
+agent_33 = DQNAgent(env, params, 128,128, noisy=True, mod=True)
+agent_34 = DQNAgent(env, params, 128,256, noisy=True, mod=True)
+agent_35 = DQNAgent(env, params, 256,256, noisy=True, mod=True)
+agent_36 = DQNAgent(env, params, 256,512, noisy=True, mod=True)
 
 
 
-training = True
+training = False
 if training:
     agent_1.train()
     # agent_2.train()
@@ -110,14 +110,15 @@ if training:
     
 
 
-loading = False
+loading = True
 if loading:
     data_to_plot = []
     
     # data_to_plot.append((load_data(agent_11.env_name + agent_11.model_name),'darkgrey', 'DQN Noisy'))
     # data_to_plot.append((load_data(agent_27.env_name + agent_24.model_name),'orange', 'Duling Noisy'))
-    data_to_plot.append((load_data(agent_25.env_name + agent_25.model_name),'red','Duling DE'))
-    data_to_plot.append((load_data(agent_25.env_name + agent_31.model_name),'green', 'DQN DN'))
+    data_to_plot.append((load_data(agent_25.env_name + agent_25.model_name),'red','1'))
+    data_to_plot.append((load_data(agent_25.env_name + agent_28.model_name),'green', '2'))
+    data_to_plot.append((load_data(agent_25.env_name + agent_19.model_name),'salmon', '3'))
     # data_to_plot.append((load_data(agent_22.env_name + agent_29.model_name),'gold', 'DQN DN'))
     # data_to_plot.append((load_data(agent_33.env_name + agent_30.model_name),'salmon', 'DQN DN'))
     plot(data_to_plot)
